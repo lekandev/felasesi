0ce3a8d3298bcc01651644a0d9702219aad0a610
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-06-09 04:35:09 +0100

    hmph

--- scripts/fela.js
+++ scripts/fela.js
@@ -4,3 +4,31 @@ const sideNav = document.querySelector(".fa-bars");
 sideNav.addEventListener("click", () => {
     sideNav.classList.toggle("fa-times");
 });
+
+// Page transitions
+barba.init({
+  transitions: [{
+    name: 'page-transition',
+    async leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    }
+  }]
+});
+// barba.init({
+//   transitions: [{
+//     name: 'page-transition',
+//     async leave() {
+//         await loaderIn()
+//     },
+//     enter() {
+//         lodaerAway()
+//     }
+//   }]
+// });
