b0c6f71c17791b0e33590d00d8fe1fd4c1d555dd
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-06-24 22:15:24 +0100

    up and going!

--- scripts/fela.js
+++ scripts/fela.js
@@ -6,18 +6,18 @@ sideNav.addEventListener("click", () => {
 });
 
 // Page transitions
-barba.init({
-  transitions: [{
-    name: 'page-transition',
-    async leave(data) {
-      return gsap.to(data.current.container, {
-        opacity: 0
-      });
-    },
-    enter(data) {
-      return gsap.from(data.next.container, {
-        opacity: 0
-      });
-    }
-  }]
-});
+// barba.init({
+//   transitions: [{
+//     name: 'page-transition',
+//     async leave(data) {
+//       return gsap.to(data.current.container, {
+//         opacity: 0
+//       });
+//     },
+//     enter(data) {
+//       return gsap.from(data.next.container, {
+//         opacity: 0
+//       });
+//     }
+//   }]
+// });
