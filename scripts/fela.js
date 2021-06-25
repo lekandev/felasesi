3a428222fc5fdcf9e88898c0b67b9368b939439d
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-06-25 12:50:24 +0100

    updates

--- scripts/fela.js
+++ scripts/fela.js
@@ -1,10 +1,20 @@
 console.log("Fela is Hot");
 const sideNav = document.querySelector(".fa-bars");
+let navigation = document.querySelector(".nav");
 
 sideNav.addEventListener("click", () => {
     sideNav.classList.toggle("fa-times");
 });
 
+window.onscroll = () => {
+    const top = window.scrollY
+    if (top >= 200) {
+        navigation.style.backgroundColor = "#111";
+    } else {
+        navigation.style.backgroundColor = "transparent";
+    }
+    
+}
 // Page transitions
 // barba.init({
 //   transitions: [{
